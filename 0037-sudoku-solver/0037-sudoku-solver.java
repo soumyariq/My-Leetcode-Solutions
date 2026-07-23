b7efed05dd4301b9ee4240c10716cdb6b0cf39fc
Soumya Mishra <118365110+soumyariq@users.noreply.github.com>
2026-07-23 12:11:46 +0530

    Time: 219 ms (65.21%), Space: 42.6 MB (86.27%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char d = '1'; d <= '9'; d++) {
+                        if (isValid(board, i, j, d)) {
+                            board[i][j] = d;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[i][j] = '.'; 
+                        }
+                    }
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+    private boolean isValid(char[][] board, int row, int col, char d) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == d) return false; 
+            if (board[row][i] == d) return false; 
+        }
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                if (board[startRow + k][startCol + l] == d) {
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+}
